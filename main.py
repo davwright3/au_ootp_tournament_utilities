bcc03c6b2d658cd4e6d6402e0112fa508454c4fd
David Wright <david.a.wright.work@gmail.com>
2025-03-28 18:59:34 -0500

    Completed basic header, footer and main frame layout for main app page.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import os
+
 import customtkinter as ctk
 import subprocess
 from configparser import ConfigParser
+import os
 
 class MainApp(ctk.CTk):
     def __init__(self):
         super().__init__()
         ctk.set_default_color_theme('themes/main_theme.json')
+
+        image_path = os.path.join("assets", "Unicorn_logo_nobg2.png")
+
+        if not os.path.exists(image_path):
+            print(f"Error, image file not found: {image_path}")
+            exit()
 
         self.settings = self.load_settings()
 
@@ -19,7 +28,16 @@ class MainApp(ctk.CTk):
         self.header_frame.grid(column=0, row=0, padx=10, pady=10, sticky='n')
 
         self.footer_frame =ctk.CTkFrame(self, height=int(int(self.settings['MainWindow']['height']) * .1), width=int(self.settings['MainWindow']['width']))
-        self.footer_frame.grid(column=0, row=1, padx=10, pady=10, sticky='s')
+        self.footer_frame.grid(column=0, row=2, padx=10, pady=10, sticky='s')
+
+        self.main_frame = ctk.CTkFrame(self, fg_color="blue", corner_radius=0)
+        self.main_frame.grid(column=0, row=1, padx= 10, sticky="nsew")
+
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=0)
+
+
 
 
 
